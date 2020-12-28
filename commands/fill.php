d2b5cb9c3192cf57aa589c54b54e3f8dfa29c7dd
Hélène Baudrand <baudrand.helene@gmail.com>
2020-12-28 10:40:05 +0100

    use config in fill.php

--- commands/fill.php
+++ commands/fill.php
@@ -2,7 +2,11 @@
 require dirname(__DIR__) . '/vendor/autoload.php';
 $faker = Faker\Factory::create('en_EN');
 
-$pdo = new PDO('mysql:dbname=phpblog;host=127.0.0.1', 'root', 'mySQLn56baud', [
+include '../config/conf.php';
+/** @var TYPE_NAME $db */
+/** @var TYPE_NAME $user */
+/** @var TYPE_NAME $password */
+$pdo = new PDO('mysql:dbname=' . $db . ';host=127.0.0.1', $user, $password, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
